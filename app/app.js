46230e4cae3c4064f07b4bcfcc2702301d557632
Sam Walton <sam@nicheprovider.com>
2017-08-20 14:52:50 -0500

    basic question page start

--- app/app.js
+++ app/app.js
@@ -5,6 +5,7 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.ProfileView',
   'myApp.GroupView',
+  'myApp.QuestionView',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
---
+++ app/profile_view/question_view.js
@@ -0,0 +1,29 @@
+'use strict';
+
+angular.module('myApp.QuestionView', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/profile_view', {
+    templateUrl: 'profile_view/question_view.html',
+    controller: 'QuestionCtrl'
+  });
+}])
+
+.controller('QuestionCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get ('https://readtheroom-6b306.firebaseio.com/users/user0.json')
+  .then ((userObj) => {
+  $scope.profile = userObj.data;
+  var mypoints = $scope.profile.askPoints;
+  })
+
+  $scope.pts = function(){
+    $scope.profile.askPoints += 1;
+
+    console.log($scope.profile.askPoints);
+    }
+  
+  })
+
+  // console.log("what's scope", $scope);
+
+}]);
