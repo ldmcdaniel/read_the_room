95f3a4eec3087ca2af584939e1b842f2ffefa4f2
Belve D. Marks <belvedmarks4@gmail.com>
2017-08-20 09:04:03 -0500

    Fixed some typos concerning routing

--- app/app.js
+++ app/app.js
@@ -3,8 +3,8 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.profileView',
-  'myApp.groupView',
+  'myApp.ProfileView',
+  'myApp.GroupView',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
