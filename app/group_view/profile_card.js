919e32b3a96cf29db4f9f38dfa38837ecba4724d
Bri Wyatt <mbriwyatt@gmail.com>
2017-08-20 10:47:10 -0500

    templates for profile cards created in group_view.html

---
+++ app/group_view/profile_card.js
@@ -0,0 +1,7 @@
+angular.
+module('myApp').
+component('profileCard', {
+  // Note: The URL is relative to our `index.html` file
+  templateUrl: 'group_view/profile_card.html',
+  controller: ...
+});
