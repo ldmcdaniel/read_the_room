36cef4ca5f06e16fff1092b4c6749ed559c30a63
Belve D. Marks <belvedmarks4@gmail.com>
2017-08-20 15:51:09 -0500

    tasklist functionalish

--- app/app.js
+++ app/app.js
@@ -12,4 +12,15 @@ config(['$locationProvider', '$routeProvider', function($locationProvider, $rout
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/profile_view'});
-}]);
+}])
+.run(function($location, FIREBASE_CONFIG) {
+  let creds = FIREBASE_CONFIG;
+  let authConfig = {
+    apiKey: creds.apiKey,
+    authDomain: creds.authDomain,
+    databaseURL: creds.databaseURL,
+    storageBucket: creds.storageBucket,
+    messagingSenderId: creds.messagingSenderId
+  };
+  firebase.initializeApp(authConfig);
+});
